238f9ca82990044bee6ef4ff6a2ae38356894bcf
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-07 23:04:27 +0530

    code correction

--- app/Http/Controllers/LibrayController.php
+++ app/Http/Controllers/LibrayController.php
@@ -9,6 +9,6 @@ class LibrayController extends Controller
     public function index()
     {
         $students = Student::all();
-        return view ('students.inde)->with('students', $students);
+        return view ('students.index')->with('students', $students);
     }
 }
