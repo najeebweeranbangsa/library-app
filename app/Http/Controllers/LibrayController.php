90decc91781b44469b255f3fd4dddc1f4419430a
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-07 23:03:44 +0530

    add index function

--- app/Http/Controllers/LibrayController.php
+++ app/Http/Controllers/LibrayController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class LibrayController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = Student::all();
+        return view ('students.inde)->with('students', $students);
+    }
 }
