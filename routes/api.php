1cdf0e899860814c902d6d74afef50069a5321b9
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-08 22:04:36 +0530

    making changes in the table

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibraryConroller;
 
-Route::get('/student',[App\Http\Controllers\LibraryConroller::class, 'index']);
+Route::get('/students',[App\Http\Controllers\LibraryConroller::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
