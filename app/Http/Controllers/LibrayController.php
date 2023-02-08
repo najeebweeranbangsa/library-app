4d94f38c4a9e4aeeb003a07994d72743792a43dd
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-08 10:53:30 +0530

    status: negative BackEnd

--- app/Http/Controllers/LibrayController.php
+++ app/Http/Controllers/LibrayController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
+
 
 class LibrayController extends Controller
 {
     public function index()
     {
-        $Student = Students::all();
-        return response() -> json($student);
+        $students = Student::all();
+        return response() -> json($students);
     }
 }
