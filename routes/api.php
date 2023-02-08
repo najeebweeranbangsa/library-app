4fa1187909011ab1ad8b5fc7f6008dec024ea60a
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-08 10:19:28 +0530

    connect api with controller

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibraryConroller;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/contact',[App\Http\Controllers\LibraryConroller::class, 'index'])
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
